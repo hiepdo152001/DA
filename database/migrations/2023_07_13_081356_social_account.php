dbdc53d0c8e56b969ba296ee6943c4a84c7e5f99
hiepdv <hiep.dv@zinza.com.vn>
2023-07-17 16:50:37 +0700

    oauth2 gg

--- database/migrations/2023_07_13_081356_social_account.php
+++ database/migrations/2023_07_13_081356_social_account.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('social_accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique()->nullable();
-            $table->string('password')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('social_id');
+            $table->string('social_provider');
+            $table->string('social_name');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
