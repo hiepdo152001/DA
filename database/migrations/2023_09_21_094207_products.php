a7db94c3c6b8d82b5d4034fe8dd018f3ac4e82a0
hiepdo <hiepdo152001@gmail.com>
2023-10-03 17:40:03 +0700

    migrate

---
+++ database/migrations/2023_09_21_094207_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\branch;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products',function(Blueprint $table){
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('price')->nullable();
+            $table->string('avatar')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('branch_id')->nullable();
+            $table->foreign('branch_id')->references('id')->on('branch');
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
